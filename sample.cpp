74f82ef131cd330c27b2c8ac535ba6ecfc60ee3c
Michael-Clark1330 <156688948+Michael-Clark1330@users.noreply.github.com>
2025-09-01 01:30:40 +0000

    Completed assignment

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,19 @@
-// YOUR NAME HERE
+// Michael Clark
 #include <iostream>
 using namespace std;
 
 int main () {
 
+    int num1, num2, sum;
+
+    cout << "Enter first integer: ";
+    cin >> num1;
+
+    cout << "Enter second integer: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+    cout << "The sum is " << sum << "." << endl;
+
+    return 0;
 }
